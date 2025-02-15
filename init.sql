b1b87bd18c2ab9e372e4293e9bcbb7e1dc1985a3
zelimkhan <z.ataev@astondevs.ru>
2025-02-15 12:35:16 +0300

    доработка Sql init

--- init.sql
+++ init.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE,
-    chat_id BIGINT NOT NULL
+    username TEXT NOT NULL,
+    chat_id BIGINT NOT NULL -- Убираем уникальность, так как пользователь может быть в нескольких чатах
 );
 
 CREATE TABLE IF NOT EXISTS reminders (
     id SERIAL PRIMARY KEY,
     text TEXT NOT NULL,
-    send_time TEXT NOT NULL
+    send_time TIME NOT NULL,
+    chat_id BIGINT NOT NULL -- Добавляем chat_id в таблицу reminders, чтобы напоминания были привязаны к чатам
 );
